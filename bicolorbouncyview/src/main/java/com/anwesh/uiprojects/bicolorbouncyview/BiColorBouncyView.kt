6d1265b9c3290f8c9fc67757187b5f1e1a773053
Anwesh43 <anweshthecool0@gmail.com>
2019-11-10 18:07:40 +0530

    adding BCBNode class

--- bicolorbouncyview/src/main/java/com/anwesh/uiprojects/bicolorbouncyview/BiColorBouncyView.kt
+++ bicolorbouncyview/src/main/java/com/anwesh/uiprojects/bicolorbouncyview/BiColorBouncyView.kt
@@ -130,4 +130,46 @@ class BiColorBouncyView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BCBNode(var i : Int, val state : State = State()) {
+
+        private var next : BCBNode? = null
+        private var prev : BCBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BCBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBCBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BCBNode {
+            var curr : BCBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
